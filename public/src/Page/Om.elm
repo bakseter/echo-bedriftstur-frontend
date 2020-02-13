1ded1d64576c700c18d17d4e445382b12e36d28c
Andreas Bakseter <andreas_tkd@hotmail.com>
2020-02-13 23:21:22 +0100

    Added hidden e-mails, and fixed the Om-page up a bit

--- public/src/Page/Om.elm
+++ public/src/Page/Om.elm
@@ -2,16 +2,25 @@ module Page.Om exposing (init, subscriptions, update, view, Model, Msg)
 
 import Html exposing (Html, div, text, img, i, a)
 import Html.Attributes exposing (class, id, src, alt, href, target, rel)
+import Html.Events
 
 type Msg
-    = None
+    = EliasMail
+    | AndreasMail
+    | TuvaMail
 
 type alias Model = 
-    Html Msg
+    { showMailElias : Bool
+    , showMailAndreas : Bool
+    , showMailTuva : Bool
+    }
 
 init : Model
 init =
-    div [] []
+    { showMailElias = False
+    , showMailAndreas = False
+    , showMailTuva = False
+    }
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -19,7 +28,13 @@ subscriptions model =
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    (model, Cmd.none)
+    case msg of
+        EliasMail ->
+            ({ model | showMailElias = True }, Cmd.none)
+        AndreasMail ->
+            ({ model | showMailAndreas = True }, Cmd.none)
+        TuvaMail ->
+            ({ model | showMailTuva = True }, Cmd.none)
 
 view : Model -> Html Msg
 view model =
@@ -27,21 +42,24 @@ view model =
         [ div [ id "om-content" ]
             [ div [ id "om-tekst" ] 
                 [ div [ class "text" ] [ text "Bedriftsturkomitéen består av tre frivillige studenter." ] ]
-                , div [ id "elias" ] [ img [ class "portrett", src "/img/elias.png", alt "Elias" ] [] ]
-                , div [ class "om-info", id "elias-info" ]
-                    [ div [ class "navn" ] [ text "Elias Djupesland" ]
-                    , div [ class "tittel" ] [ text "Bachelor, datateknologi" ]
-                    ]
-                , div [ id "andreas" ] [ img [ class "portrett", src "/img/andreas.png", alt "Andreas" ] [] ]
-                , div [ class "om-info", id "andreas-info" ] 
-                    [ div [ class "navn" ] [ text "Andreas Salhus Bakseter" ]
-                    , div [ class "tittel" ] [ text "Bachelor, Datateknologi" ]
-                    ]
-                , div [ id "tuva" ] [ img [ class "portrett", src "/img/tuva.png", alt "Tuva" ] [] ]
-                , div [ class "om-info", id "tuva-info" ] 
-                    [ div [ class "navn" ] [ text "Tuva Kvalsøren" ]
-                    , div [ class "tittel" ] [ text "Bachelor, IMØ" ]
-                    ]
+            , div [ id "elias" ] [ img [ class "portrett", src "/img/elias.png", alt "Elias" ] [] ]
+            , div [ class "om-info", id "elias-info" ]
+                [ div [ class "navn" ] [ text "Elias Djupesland" ]
+                , div [ class "tittel" ] [ text "Bachelor, datateknologi" ]
+                , div [ class (Tuple.first (getMail model EliasMail)), Html.Events.onClick EliasMail ] [ text (Tuple.second (getMail model EliasMail)) ]
+                ]
+            , div [ id "andreas" ] [ img [ class "portrett", src "/img/andreas.png", alt "Andreas" ] [] ]
+            , div [ class "om-info", id "andreas-info" ] 
+                [ div [ class "navn" ] [ text "Andreas Salhus Bakseter" ]
+                , div [ class "tittel" ] [ text "Bachelor, datateknologi" ]
+                , div [ class (Tuple.first (getMail model AndreasMail)), Html.Events.onClick AndreasMail ] [ text (Tuple.second (getMail model AndreasMail)) ]
+                ]
+            , div [ id "tuva" ] [ img [ class "portrett", src "/img/tuva.png", alt "Tuva" ] [] ]
+            , div [ class "om-info", id "tuva-info" ] 
+                [ div [ class "navn" ] [ text "Tuva Kvalsøren" ]
+                , div [ class "tittel" ] [ text "Bachelor, IMØ" ]
+                , div [ class (Tuple.first (getMail model TuvaMail)), Html.Events.onClick TuvaMail ] [ text (Tuple.second (getMail model TuvaMail)) ]
+                ]
             ]
         , div [ id "om-links" ]
             [ a [ target "_blank"
@@ -55,3 +73,22 @@ view model =
                  ] [ i [ class "fa fa-github" ] [] ]
             ]
         ]
+
+getMail : Model -> Msg -> (String, String)
+getMail model msg =
+    case msg of
+        EliasMail ->
+            if model.showMailElias then
+                ("mail", "elias.djupesland@echo.uib.no")
+            else
+                ("hidden-mail", "Trykk for mail")
+        AndreasMail ->
+            if model.showMailAndreas then
+                ("mail", "andreas.bakseter@echo.uib.no")
+            else
+                ("hidden-mail", "Trykk for mail")
+        TuvaMail ->
+            if model.showMailTuva then
+                ("mail", "tuva.kvalsoren@echo.uib.no")
+            else
+                ("hidden-mail", "Trykk for mail")
