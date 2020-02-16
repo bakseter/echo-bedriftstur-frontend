c919c8e6645083fbd967b76c09542e251fc009b1
Andreas Bakseter <andreas_tkd@hotmail.com>
2020-02-16 20:04:56 +0100

    Refactoring and automated Travis CI build

--- public/src/Page/Bedrifter.elm
+++ public/src/Page/Bedrifter.elm
@@ -1,4 +1,4 @@
-module Page.Bedrifter exposing (init, subscriptions, update, view, Model, Msg)
+module Page.Bedrifter exposing (init, subscriptions, update, view, Model, Msg, slideInStyle)
 
 import Html exposing (Html, div, span, text, a, img, h1, h2, p)
 import Html.Attributes exposing (class, id, target, rel, href, src, alt)
@@ -110,52 +110,22 @@ update msg model =
         Tick time ->
             (model, Task.perform GotViewport Browser.Dom.getViewport)
         SlideInMnemonic ->
-            let newStyle = Animation.interrupt
-                            [ Animation.toWith ease
-                                [ Animation.opacity 1.0
-                                , Animation.left (percent 40) 
-                                ]
-                            ] model.mnemonicAnim
+            let newStyle = slideInStyle model.mnemonicAnim
             in ({ model | mnemonicAnim = newStyle }, Cmd.none)
         SlideInComputas ->
-            let newStyle = Animation.interrupt
-                            [ Animation.toWith ease
-                                [ Animation.opacity 1.0
-                                , Animation.left (percent 40)
-                                ]
-                            ] model.computasAnim
+            let newStyle = slideInStyle model.computasAnim
             in ({ model | computasAnim = newStyle }, Cmd.none)
         SlideInTba ->
-            let newStyle = Animation.interrupt
-                            [ Animation.toWith ease
-                                [ Animation.opacity 1.0
-                                , Animation.left (percent 40)
-                                ]
-                            ] model.tbaAnim
+            let newStyle = slideInStyle model.tbaAnim
             in ({ model | tbaAnim = newStyle }, Cmd.none)
         SlideInKnowit ->
-            let newStyle = Animation.interrupt
-                            [ Animation.toWith ease
-                                [ Animation.opacity 1.0
-                                , Animation.left (percent 40)
-                                ]
-                            ] model.knowitAnim
+            let newStyle = slideInStyle model.knowitAnim
             in ({ model | knowitAnim = newStyle }, Cmd.none)
         SlideInDnb ->
-            let newStyle = Animation.interrupt
-                            [ Animation.toWith ease
-                                [ Animation.opacity 1.0
-                                , Animation.left (percent 40)
-                                ]
-                            ] model.dnbAnim
+            let newStyle = slideInStyle model.dnbAnim
             in ({ model | dnbAnim = newStyle }, Cmd.none)
         SlideInBekk ->
-            let newStyle = Animation.interrupt 
-                            [ Animation.toWith ease
-                                [ Animation.opacity 1.0
-                                , Animation.left (percent 40) 
-                                ] 
-                            ] model.bekkAnim
+            let newStyle = slideInStyle model.bekkAnim
             in ({ model | bekkAnim = newStyle }, Cmd.none)
         AnimMnemonic anim ->
             ({ model | mnemonicAnim = Animation.update anim model.mnemonicAnim }, Cmd.none)
@@ -178,30 +148,38 @@ view model =
             [ span (Animation.render model.mnemonicAnim ++ [ class "logo-item", id "mnemonic" ])
                 [ a [ target "_blank", rel "noopener noreferrer", href "https://www.mnemonic.no" ]
                     [ img  [ class "bed-logo", src "/img/mnemonic.png", alt "mnemonic" ] [] ]
-                , p [ class "bed-text" ] [ text "mnemonic hjelper virksomheter med å administrere og håndtere sine sikkerhetsrisikoer, beskytte sine data og forsvare seg mot trusler fra Internett. " ]
-                , p [ class "bed-text" ] [ text "Vårt ekspertteam av sikkerhetskonsulenter, produktspesialister, trusseletterforskere, team av hendelseshåndterere og etiske hackere, kombinert med vår Argus sikkerhetsplattform sikrer at vi ligger i forkant av avanserte angrep fra Internett og beskytter våre kunder fra nye trusler." ]
+                , p [ class "bed-text" ]
+                    [ text "mnemonic hjelper virksomheter med å administrere og håndtere sine sikkerhetsrisikoer, beskytte sine data og forsvare seg mot trusler fra Internett. " ]
+                , p [ class "bed-text" ]
+                    [ text "Vårt ekspertteam av sikkerhetskonsulenter, produktspesialister, trusseletterforskere, team av hendelseshåndterere og etiske hackere, kombinert med vår Argus sikkerhetsplattform sikrer at vi ligger i forkant av avanserte angrep fra Internett og beskytter våre kunder fra nye trusler." ]
                 ]
             , span (Animation.render model.computasAnim ++ [ class "logo-item", id "computas" ])
                 [ a [ target "_blank", rel "noopener noreferrer", href "https://computas.com" ]
                     [ img  [ class "bed-logo", src "/img/computas.png", alt "Computas" ] [] ]
-                , p [ class "bed-text" ] [ text "Computas er en norsk leverandør av IT-løsninger og rådgivningstjenester innen teknologisk innovasjon. Vi leverer verdiskapende og samfunnsnyttige løsninger til både offentlig og privat sektor, og har særlig spisskompetanse innenfor offentlig forvaltning, justis, tilsyn, helse, logistikk, olje og gass. Vi jobber med alt fra apper som redder liv og prisvinnende saksbehandlingsløsninger, til dataanalyse, kunstig intelligens og omfattende, skybaserte innovasjonsprosjekter." ]
+                , p [ class "bed-text" ]
+                    [ text "Computas er en norsk leverandør av IT-løsninger og rådgivningstjenester innen teknologisk innovasjon. Vi leverer verdiskapende og samfunnsnyttige løsninger til både offentlig og privat sektor, og har særlig spisskompetanse innenfor offentlig forvaltning, justis, tilsyn, helse, logistikk, olje og gass. Vi jobber med alt fra apper som redder liv og prisvinnende saksbehandlingsløsninger, til dataanalyse, kunstig intelligens og omfattende, skybaserte innovasjonsprosjekter." ]
                 ]
             , span (Animation.render model.knowitAnim ++ [ class "logo-item", id "knowit" ])
                 [ a [ target "_blank", rel "noopener noreferrer", href "https://www.knowit.no" ]
                     [ img  [ class "bed-logo", src "/img/knowit.png", alt "Knowit" ] [] ]
-                , p [ class "bed-text" ] [ text "Knowit er et konsulentselskap som, i den stadig raskere digitaliseringen, skaper unike kundeverdier gjennom å tilby grenseoverskridende leveranser fra de tre forretningsområdene Experience, Insight og Solutions. Det er evnen til å kombinere kompetanse innen design og kommunikasjon, management consulting og IT som skiller oss fra andre konsulentfirmaer. Vår kultur preges av åpenhet, forståelse for kundens forretninger, høy spesialistkompetanse og en vilje til å utvikles kontinuerlig." ]
+                , p [ class "bed-text" ]
+                    [ text "Knowit er et konsulentselskap som, i den stadig raskere digitaliseringen, skaper unike kundeverdier gjennom å tilby grenseoverskridende leveranser fra de tre forretningsområdene Experience, Insight og Solutions. Det er evnen til å kombinere kompetanse innen design og kommunikasjon, management consulting og IT som skiller oss fra andre konsulentfirmaer. Vår kultur preges av åpenhet, forståelse for kundens forretninger, høy spesialistkompetanse og en vilje til å utvikles kontinuerlig." ]
                 ]
             , span (Animation.render model.dnbAnim ++ [ class "logo-item", id "dnb" ])
                 [ a [ target "_blank", rel "noopener noreferrer", href "https://www.dnb.no" ]
                     [ img  [ id "dnb-logo", class "bed-logo", src "/img/dnb.png", alt "DNB" ] [] ]
-                , p [ class "bed-text" ] [ text "DNB er mer enn bare en bank. Vår ambisjon er å være et av Europas ledende teknologiselskaper. Bank- og finansbransjen gjennomgår en enorm forandring, og kundeadferden endrer seg raskt. En kritisk del av denne transformasjonen er å etablere raskere og mer effektive måter å jobbe på." ]
-                , p [ class "bed-text" ] [ text "Ved hjelp av metoder som \"design thinking\" og \"lean startup\" skaper vi tjenester som gir de best kundeopplevelsene. Vi jobber hele tiden med å bygge en kultur av gjensidig respekt, læring og åpenhet mot våre kunder og samfunn. Vi investerer i våre folk, fordi det er de som driver forandringen." ]
+                , p [ class "bed-text" ]
+                    [ text "DNB er mer enn bare en bank. Vår ambisjon er å være et av Europas ledende teknologiselskaper. Bank- og finansbransjen gjennomgår en enorm forandring, og kundeadferden endrer seg raskt. En kritisk del av denne transformasjonen er å etablere raskere og mer effektive måter å jobbe på." ]
+                , p [ class "bed-text" ]
+                    [ text "Ved hjelp av metoder som \"design thinking\" og \"lean startup\" skaper vi tjenester som gir de best kundeopplevelsene. Vi jobber hele tiden med å bygge en kultur av gjensidig respekt, læring og åpenhet mot våre kunder og samfunn. Vi investerer i våre folk, fordi det er de som driver forandringen." ]
                 ]
             , span (Animation.render model.bekkAnim ++ [ class "logo-item", id "bekk" ])
                 [ a [ target "_blank", rel "noopener noreferrer", href "https://www.bekk.no" ]
                     [ img  [ class "bed-logo", src "/img/bekk.png", alt "Bekk" ] [] ]
-                , p [ class "bed-text" ] [ text " I Bekk er vi flinke til å bygge hverandre opp, utfordre hverandre og, ikke minst, de vi jobber med. Vi motiveres av å drive utviklingen fremover og heve standarden for hva som anses for å være godt levert. Vi inspireres av å spre kunnskap om hva som virker, og hvorfor." ]
-                , p [ class "bed-text" ] [ text "Vi har ingen tro på strømlinjeformede arbeidsplasser. Kreativ frihet og muligheten til å påvirke egen arbeidsdag, sette dagsorden og styre utviklingen av selskapet, det er mener vi viktigere enn å følge opptråkkede stier. Initiativ og engasjement er hardkodet i ryggraden vår og har skapt en sterk og inkluderende kultur." ]
+                , p [ class "bed-text" ]
+                    [ text " I Bekk er vi flinke til å bygge hverandre opp, utfordre hverandre og, ikke minst, de vi jobber med. Vi motiveres av å drive utviklingen fremover og heve standarden for hva som anses for å være godt levert. Vi inspireres av å spre kunnskap om hva som virker, og hvorfor." ]
+                , p [ class "bed-text" ]
+                    [ text "Vi har ingen tro på strømlinjeformede arbeidsplasser. Kreativ frihet og muligheten til å påvirke egen arbeidsdag, sette dagsorden og styre utviklingen av selskapet, det er mener vi viktigere enn å følge opptråkkede stier. Initiativ og engasjement er hardkodet i ryggraden vår og har skapt en sterk og inkluderende kultur." ]
                 ]
             , span (Animation.render model.tbaAnim ++ [ class "logo-item", id "tba" ])
                 [ a [ target "_blank", rel "noopener noreferrer", href "" ]
@@ -236,3 +214,12 @@ ease =
     Animation.spring
         { stiffness = 80
         , damping = 20 }
+
+slideInStyle : Animation.State -> Animation.State
+slideInStyle anim =
+    Animation.interrupt
+        [ Animation.toWith ease
+            [ Animation.opacity 1.0
+            , Animation.left (percent 40)
+            ]
+        ] anim
