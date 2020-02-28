9436107c01637bc4ce4917ec6dd3123803d9fc11
Andreas Bakseter <andreas_tkd@hotmail.com>
2020-02-28 16:41:53 +0100

    :pencil: Updated README
    Info and local setup

--- README.md
+++ README.md
@@ -2,4 +2,25 @@
 
 [![Build Status](https://travis-ci.org/bakseter/echo-bedriftstur.svg?branch=master)](https://travis-ci.org/bakseter/echo-bedriftstur)
 
-Website of echo - Fagutvalget for informatikk's student trip to Oslo.
+### Info
+
+Website of echo bedriftstur - a student trip to Oslo organized by volunteers from _echo - Fagutvalget for Informatikk_ at the University of Bergen.
+The website is written in Elm and hosted using Firebase.
+
+### Local setup
+
+**1. Clone the git repository**
+    
+    git clone https://github.com/bakseter/echo-bedriftstur
+
+**2. Install dependencies**
+
+    sudo npm install -g firebase-tools elm
+
+**4. Build the project**
+    
+    elm make src/Main.elm --output public/main.js
+
+**3. Host the website locally**
+
+    firebase serve
---
+++ build/precache-manifest.4954d95b318a21d7d023741a3aaf77b9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e492ebcb6f6cb5d422d6aa11ceeb2a5",
+    "url": "/.eslintrc.json"
+  },
+  {
+    "revision": "ff221779799685303bc59ed0b1cddd64",
+    "url": "/css/style.css"
+  },
+  {
+    "revision": "077115b34d5d71894eb17647db6f5ac3",
+    "url": "/img/andreas.png"
+  },
+  {
+    "revision": "296df87678f9e03cdb4f687fcead9680",
+    "url": "/img/bekk.png"
+  },
+  {
+    "revision": "a387aa33319c821f69f6697b58399ed7",
+    "url": "/img/computas.png"
+  },
+  {
+    "revision": "659d1a9ec78168f7713021d675b58f7b",
+    "url": "/img/dnb.png"
+  },
+  {
+    "revision": "198d45239eb9d68e778c560746705831",
+    "url": "/img/echo-favicon-192.png"
+  },
+  {
+    "revision": "39e3d65cc29ed116f5bec86359524f0b",
+    "url": "/img/echo-favicon.png"
+  },
+  {
+    "revision": "3fd1e6e147dac54ecb66e973767f2989",
+    "url": "/img/echo-logo-very-wide.png"
+  },
+  {
+    "revision": "a3eb8e262ca7e461ca35f4f24f18d570",
+    "url": "/img/elias.png"
+  },
+  {
+    "revision": "26915c6fb0be8323744efe5fd2b7897d",
+    "url": "/img/knowit.png"
+  },
+  {
+    "revision": "f4f2e77c92ff2350ed48cfa0e97b4de6",
+    "url": "/img/mnemonic.png"
+  },
+  {
+    "revision": "7c239456ddfcefc71049cab30c7f0175",
+    "url": "/img/tuva.png"
+  },
+  {
+    "revision": "29441e6a5b0b7a296f817c472384a3b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "628f8e4f4d6855dff8a7af95658342a9",
+    "url": "/main.js"
+  },
+  {
+    "revision": "854634ee0d874537d0049b0e6c0143b2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "5e0bd1c281a62a380d7a948085bfe2d1",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ab7214cbbc872e0c1ad1",
+    "url": "/static/js/main.ab7214cb.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "6ed50bfada2fbb364135",
+    "url": "/static/js/vendors~main.6ed50bfa.chunk.js"
+  }
+]);
