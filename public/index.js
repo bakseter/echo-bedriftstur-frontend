1be069bdf3baf3610096bf0bc2be849e2ea3e8a0
Andreas Bakseter <andreas_tkd@hotmail.com>
2021-06-13 22:41:31 +0200

    🔇 Remove output file

--- public/index.js
+++
@@ -1,6 +0,0 @@
-import { Elm } from './Main.elm';
-
-Elm.Main.init ({
-    node: document.getElementById('elm'),
-    flags: { apiKey: 'text', width: window.innerWidth, height: window.innerHeight }
-});
